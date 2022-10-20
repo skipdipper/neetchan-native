80aec99d2dad70343c4db7a63731987ac3813084
skipdipper <morgan-yyh@protonmail.com>
2022-10-20 21:40:40 +1000

    Refactor CatalogSortbyMenu to display Radio buttons instead of Select

--- src/features/catalog/CatalogSortByMenu.tsx
+++ src/features/catalog/CatalogSortByMenu.tsx
@@ -1,15 +1,37 @@
+import { useState, useTransition } from "react";
+import { StyleSheet, View } from "react-native";
 import { CatalogPost } from "../../shared/types";
-import Option from "../ui/select/Option";
-import Select from "../ui/select/Select";
+import { ModalVisibilityContextInterface, useModalVisibility } from "../ui/modal/ModalVisibilityContext";
+import RadioListTile from "../ui/select/RadioListTile";
 import { CatalogContextInterface, useCatalogContext } from "./CatalogContext";
 
+const Sort = {
+    BUMP: 'bump',
+    REPLY_COUNT: 'reply',
+    IMAGE_COUNT: 'image',
+    NEWEST: 'newest',
+    OLDEST: 'oldest',
+    LAST_REPLY: 'lastReply'
+} as const;
+
 export default function CatalogSortByMenu() {
+    const [isPending, startTransition] = useTransition();
     const { data, setData } = useCatalogContext() as CatalogContextInterface;
+    const [checked, setChecked] = useState(Sort.BUMP);
+
+    const { modalRef } = useModalVisibility() as ModalVisibilityContextInterface;
 
     const handleChange = (value: any) => {
         console.log('Catalog SortBy', value);
-        const sorted = sortBy(data, value);
-        setData(sorted);
+        // Urgent update Radio Icon checked status
+        setChecked(value);
+        // TODO: Fix transition not deferring 
+        modalRef.current.closeModal();
+        // Transition update, alternative to flushSync
+        startTransition(() => {
+            const sorted = sortBy(data, value);
+            setData(sorted);
+        });
     }
 
     const sortBy = (catalog: CatalogPost[], sortBy: string, order: 'asc' | 'dsc' = 'dsc'): CatalogPost[] => {
@@ -33,7 +55,7 @@ export default function CatalogSortByMenu() {
         }
 
         if (!sortFns.hasOwnProperty(sortBy)) {
-            console.log('sortBy key not found');
+            console.error('sortBy key not found');
             return catalog;
         }
 
@@ -42,16 +64,26 @@ export default function CatalogSortByMenu() {
 
     // TODO: retrieve previous sortBy order from mmkv
     return (
-        <Select
-            value='bump'
-            onChanged={handleChange}
-        >
-            <Option value='bump'>Bump order</Option>
-            <Option value='reply'>Reply count</Option>
-            <Option value='image'>Image count</Option>
-            <Option value='newest'>Newest</Option>
-            <Option value='oldest'>Oldest</Option>
-            <Option value='lastReply'>Latest reply</Option>
-        </Select>
+        <View style={styles.container}>
+            <RadioListTile title="Bump order" value={Sort.BUMP} groupValue={checked} onChanged={handleChange} />
+            <RadioListTile title="Reply count" value={Sort.REPLY_COUNT} groupValue={checked} onChanged={handleChange} />
+            <RadioListTile title="Image count" value={Sort.IMAGE_COUNT} groupValue={checked} onChanged={handleChange} />
+            <RadioListTile title="Newest" value={Sort.NEWEST} groupValue={checked} onChanged={handleChange} />
+            <RadioListTile title="Oldest" value={Sort.OLDEST} groupValue={checked} onChanged={handleChange} />
+            <RadioListTile title="Latest reply" value={Sort.LAST_REPLY} groupValue={checked} onChanged={handleChange} />
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        margin: 10,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        backgroundColor: '#fff',
+    }
+});
---
+++ src/features/ui/select/Radio.tsx
@@ -0,0 +1,33 @@
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import IconButton from '../IconButton';
+
+export type RadioProps<T> = {
+    value: T;
+    groupValue: T;
+    onChanged?: (value: T) => void;
+}
+
+export default function Radio<T>({ value, groupValue, onChanged }: RadioProps<T>) {
+    const handleChange = () => {
+        if (groupValue === value) return;
+        onChanged?.(value);
+    }
+
+    if (value === groupValue) {
+        return (
+            <IconButton
+                onPress={handleChange}
+                name="radio-button-checked"
+                size={24}
+            />
+        );
+    }
+
+    return (
+        <IconButton
+            onPress={handleChange}
+            name="radio-button-unchecked"
+            size={24}
+        />
+    );
+}
